8dbecba6b1d7a04bce6a5b96d75eb4d1cb88f7ba
Anna Hallenberg <anna.thorsen.hallenberg@lego.com>
2022-04-21 18:53:35 +0200

    made K and KG, as well as a multiply matrix

--- badbitchesonly/ciphertext_only_attack_test.go
+++ badbitchesonly/ciphertext_only_attack_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	_ "fmt"
+	"reflect"
 	_ "reflect"
 	"testing"
 
@@ -10,7 +11,7 @@ import (
 )
 
 func TestCreate_G_matrix(t *testing.T) {
-	G := Create_G_matrix()
+	G := CreateGMatrix()
 
 	// fmt.Printf("G Matrix: %d \n", G)
 	for i := 0; i < 184; i++ {
@@ -19,4 +20,51 @@ func TestCreate_G_matrix(t *testing.T) {
 			fmt.Printf("G[%d][%d] er 0 men burde være 1\n", i, i)
 		}
 	}
+}
+
+func TestMultiplyMatrix(t *testing.T) {
+	A := make([][]int, 3)
+
+	A[0] = []int{0, 1, 0, 1, 0}
+	A[1] = []int{0, 0, 0, 1, 1}
+	A[2] = []int{1, 1, 1, 0, 0}
+
+	B := make([][]int, 5)
+
+	B[0] = []int{1, 1, 1}
+	B[1] = []int{0, 0, 0}
+	B[2] = []int{1, 0, 1}
+	B[3] = []int{0, 1, 0}
+	B[4] = []int{0, 1, 1}
+
+	res := MultiplyMatrix(A, B)
+	shouldBe := make([][]int, 3)
+	shouldBe[0] = []int{0, 1, 0}
+	shouldBe[1] = []int{0, 0, 1}
+	shouldBe[2] = []int{0, 1, 0}
+
+	if !reflect.DeepEqual(res, shouldBe) {
+		t.Fail()
+		t.Logf("The result is not correct. Res is: %d", res)
+	}
+}
+
+func TestMultiplyMatrix2(t *testing.T) {
+	A := CreateKgMatrix()
+	B := CreateGMatrix()
+
+	res := MultiplyMatrix(A, B)
+
+	shouldBe := make([][]int, 272)
+	for i := 0; i < 272; i++ {
+		shouldBe[i] = make([]int, 184)
+	}
+	fmt.Printf("Res size: %d x %d \n", len(res), len(res[0]))
+	fmt.Printf("ShouldBe size: %d x %d \n", len(shouldBe), len(shouldBe[0]))
+
+	if !reflect.DeepEqual(res, shouldBe) {
+		t.Fail()
+		t.Logf("The result is not correct. Res is: %d", res)
+	}
+
 }
--- badbitchesonly/ciptertext_only_attack.go
+++ badbitchesonly/ciptertext_only_attack.go
@@ -1,37 +1,78 @@
 package main
 
-func Create_G_matrix() [][]int {
+import "fmt"
+
+func CreateGMatrix() [][]int {
 	// Make first slice: 184 columns
-	G := make([][]int, 184)
+	G := make([][]int, 456)
 
 	// Make 184 slices of length 456
-	for i := 0; i < 184; i++ {
-		col_slice := make([]int, 456)
+	for i := 0; i < 456; i++ {
+		colSlice := make([]int, 184)
 
 		// Set diagonal to 1
-		col_slice[i] = 1
+		if i < 184 {
+			colSlice[i] = 1
+		}
 
-		G[i] = col_slice
+		G[i] = colSlice
 	}
 
 	return G
 }
 
-func Create_KG_matrix() [][]int {
+func CreateKgMatrix() [][]int {
 	// Make first slice: 184 columns
-	KG := make([][]int, 456)
+	KG := make([][]int, 272)
 
 	// Make 184 slices of length 456
-	for i := 0; i < 456; i++ {
-		col_slice := make([]int, 272)
+	for i := 0; i < 272; i++ {
+		colSlice := make([]int, 456)
 
 		// Set diagonal to 1 after 184
 		if i > 184 && i < 272 {
-			col_slice[i] = 1
+			colSlice[i] = 1
 		}
 
-		KG[i] = col_slice
+		KG[i] = colSlice
 	}
 
 	return KG
+}
+
+func MultiplyMatrix(A [][]int, B [][]int) [][]int {
+	noRows := len(A) // m
+	fmt.Printf("Number of rows in first matrix: %d\n", noRows)
+	noCol := len(A[0]) // n
+	fmt.Printf("Number of col in first matrix: %d\n", noCol)
+
+	secNoRows := len(B) // p
+	fmt.Printf("Number of rows in second matrix: %d\n", secNoRows)
+	secNoCol := len(B[0]) // q
+	fmt.Printf("Number of columns in second matrix: %d\n", secNoCol)
+
+	if noCol != secNoRows {
+		fmt.Println("Error: The matrix cannot be multiplied")
+	}
+
+	// Create result matrix after multiplication
+	res := make([][]int, noRows)
+
+	// Initialize inner slices
+	for i := 0; i < noRows; i++ {
+		res[i] = make([]int, secNoCol)
+	}
+
+	midRes := 0
+	for i := 0; i < noRows; i++ {
+		for j := 0; j < secNoCol; j++ {
+			for k := 0; k < secNoRows; k++ {
+				midRes = midRes ^ (A[i][k] * B[k][j])
+			}
+			res[i][j] = midRes
+			midRes = 0
+		}
+	}
+
+	return res
 }
