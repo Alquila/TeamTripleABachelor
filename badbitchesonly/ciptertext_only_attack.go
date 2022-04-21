e532d987f8d678d1cb3e495fc0aa74c7801d9785
Anna Hallenberg <anna.thorsen.hallenberg@lego.com>
2022-04-21 14:01:21 +0200

    Updated method names and created G matrix

--- badbitchesonly/ciptertext_only_attack.go
+++ badbitchesonly/ciptertext_only_attack.go
@@ -1,5 +1,22 @@
 package main
 
-func new_Gauss() {
+func Create_G_matrix() [][]int {
+	// Make first slice: 184 columns
+	G := make([][]int, 184)
 
+	// Make 184 slices of length 456
+	for i := 0; i < 184; i++ {
+		col_slice := make([]int, 456)
+
+		// Set diagonal to 1
+		col_slice[i] = 1
+
+		G[i] = col_slice
+	}
+
+	return G
+}
+
+func new_Gauss() {
+	
 }
